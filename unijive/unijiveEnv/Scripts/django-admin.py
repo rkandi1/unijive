4ace3c0db34c4d60ca8d7ac7a3110699f3e91beb
rkandi1 <kandirohan1@gmail.com>
2018-04-24 19:27:45 -0400

    Add files via upload

---
+++ unijive/unijiveEnv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\heart\onedrive\documents\github\unijive\unijiveenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
